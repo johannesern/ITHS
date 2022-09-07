ad4449d2bd8aa1cc43c1fed3f89bc6ff1af23f47
Johannes Ragnarsson <johannes.ragnarsson@gmail.com>
2022-09-07 13:58:04 +0200

    array exercise

---
+++ Array övningsuppgift/Array övningsuppgift/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Array_övningsuppgift
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //   1.
+            //string[] cars = { "Ford", "Cadillac", "Audi" };
+            //Array.Sort(cars);
+            //foreach (string car in cars)
+            //{
+            //    Console.WriteLine(car);
+            //}
+
+            //   2.
+            //int[] numbers = { 12, 50, 72 };
+            //int sum = 0;
+            //foreach(int number in numbers)
+            //{
+            //    sum += number;
+            //}
+            //Console.WriteLine(sum);
+
+            //   3.
+            //string[] strings = { "Sagan om Ringen", "Die Hard", "Nyckeln till Frihet", "Star Wars", "Harry Potter" };
+            //Array.Sort(strings);
+            //foreach(string s in strings)
+            //{
+            //    Console.WriteLine(s);
+            //}
+
+            //   4.
+            //string[,] array = { { "199", "Harry Potter" } , { "99", "En gammal pocket" }, { "399", "Sagan om Ringen" }, };
+            //for (int i = 0; i < array.Length/2; i++)
+            //{
+            //    Console.WriteLine(array[i, 0]);
+            //    Console.WriteLine(array[i, 1]);             
+            //}
+        }
+    }
+}
