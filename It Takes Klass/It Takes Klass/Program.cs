b8ef4e9de508603c0737690991503a648f19cbe7
Johannes Ragnarsson <johannes.ragnarsson@gmail.com>
2022-09-08 21:08:33 +0200

    testing

---
+++ It Takes Klass/It Takes Klass/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace It_Takes_Klass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WebsiteGenerator websiteGenerator = new WebsiteGenerator();
+
+            Console.WriteLine("Vad heter klassen:");
+            string className = Console.ReadLine();
+
+            Console.WriteLine("Vilka kurser ska klassen gå:");
+            string courses = Console.ReadLine();
+
+            Console.WriteLine("Hur många meddelande vill ni skriva till klassen:");
+            int numberOfMessages = int.Parse(Console.ReadLine());
+            List<string> classMessages = websiteGenerator.Messages(numberOfMessages);
+            
+            websiteGenerator.HTMLbuilder(className, courses, classMessages);
+
+        }
+    }
+}
---
+++ It Takes Klass/It Takes Klass/WebsiteGenerator.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace It_Takes_Klass
+{
+    internal class WebsiteGenerator
+    {
+        public string HTMLtop = "<!DOCTYPE>\n<html>\n<body>\n<main>";
+        public string HTMLbottom = "</main>\n</body>\n</html>";
+
+        public void HTMLbuilder(string className, string courses, List<string> classMessages)
+        {
+            Console.Clear();
+            Console.WriteLine(HTMLtop);
+            WelcomeClass(className);
+            foreach(string message in classMessages)
+            {
+                Console.WriteLine(message);
+            }
+            Courses(courses);
+            Console.WriteLine(HTMLbottom);
+        }
+
+        public void WelcomeClass(string className)
+        {
+            Console.WriteLine($"<h1>Välkomna {className}!</h1>");
+        }
+
+        public void Courses(string courses)
+        {
+            string[] strings = courses.Split(" ");
+            Console.WriteLine("<p>Ni kommer att läsa följande kurser:</p>");
+            for (int i = 0; i < strings.Length; i++)
+            {
+                Console.WriteLine($"<p>- {strings[i]}</p>");
+            }
+        }
+        public List<string> Messages(int classMessages)
+        {
+            Console.Clear();
+            List<string> messages = new List<string>();
+            for (int i = 0; i < classMessages; i++)
+            {
+                Console.WriteLine($"Meddelande {i + 1}: ");
+                string j = Console.ReadLine();
+                messages.Add($"<p>{j}</p>");
+            }
+            return messages;
+        }
+
+        //public void HTMLtop()
+        //{
+        //    Console.WriteLine("<!DOCTYPE>\n<html>\n<body>\n<main>");
+        //}
+
+        //public void HTMLbottom()
+        //{
+        //    Console.WriteLine("</main>\n</body>\n</html>");
+        //}
+    }
+}
