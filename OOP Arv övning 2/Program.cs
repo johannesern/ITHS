e82608d13a109ae7148518307c980871574b5ed0
[Johannes Ragnarsson] <[johannes.ragnarsson@iths.se]>
2022-09-16 15:43:40 +0200

    added OOP Arv övning 2

---
+++ OOP Arv övning 2/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace OOP_Arv_övning_2
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Industrilokal industrilokal = new Industrilokal(11, 5, 25000);
+			industrilokal.antalKvadratmeter = 1200.5f;
+			industrilokal.address = "Industrivägen 1";
+			Console.WriteLine($"De nya industrilokalen är {industrilokal.höjd} meter hög.\n" +
+							  $"Den har {industrilokal.antalRum} rum.\n" + 
+							  $"Månadskostnad: {industrilokal.månadsHyra}:-\n" + 
+							  $"Yta: {industrilokal.antalKvadratmeter} kvm\n" + 
+							  $"Lokalen ligger på {industrilokal.address} i någon stad.");
+		}
+	}
+
+	public class Fastighet
+	{
+		public double antalKvadratmeter;
+		public string address;
+	}
+
+	public class Industrilokal : Fastighet
+	{
+		public int höjd;
+		public int antalRum;
+		public int månadsHyra;
+
+		public Industrilokal(int höjd, int antalRum, int månadsHyra)
+		{
+			this.höjd = höjd;
+			this.antalRum = antalRum;
+			this.månadsHyra = månadsHyra;
+		}
+	}
+}
