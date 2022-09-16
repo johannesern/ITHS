6206e5788a48bde7715f606e7a76fbb8a3b39c54
[Johannes Ragnarsson] <[johannes.ragnarsson@iths.se]>
2022-09-16 11:40:21 +0200

    added Metoder 1, 2, 3, 4, 5

---
+++ Metoder 5/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Metoder_5
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string[] myArray = new string[] { "Jag", "gillar", "att", "köra", "motorcykel!" };
+			string whatDoYouLike = ConcatThisArray(myArray);
+			Console.WriteLine(whatDoYouLike);
+		}
+
+		public static string ConcatThisArray(string[] myArray)
+		{
+			string concatedString = "";
+			foreach(string str in myArray)
+			{
+				concatedString += str + " ";
+				concatedString.Trim();
+			}
+			return concatedString;
+		}
+	}
+}
