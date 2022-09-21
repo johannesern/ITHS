67ac6442e510e9f65245ac442612ffaa29b3ade9
[Johannes Ragnarsson] <[johannes.ragnarsson@iths.se]>
2022-09-21 07:20:44 +0200

    added Extension Methods 5

---
+++ Extension Methods 5/Program.cs
@@ -0,0 +1,25 @@
+﻿using Utilities;
+
+namespace Extension_Methods_5
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string test = "5";
+			Console.WriteLine(test.IsNumeric());
+		}
+	}
+}
+
+namespace Utilities
+{
+	public static class MyUtilities
+	{
+		public static bool IsNumeric(this string input)
+		{
+			bool isItNumeric = int.TryParse(input, out int value);
+			return isItNumeric;
+		}
+	}
+}
