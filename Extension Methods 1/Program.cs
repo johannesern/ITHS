3cefdc211911d6330ead9ae6a7ea2882c80c4bf6
[Johannes Ragnarsson] <[johannes.ragnarsson@iths.se]>
2022-09-21 06:56:52 +0200

    added Extensions Methods 1

---
+++ Extension Methods 1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using Utilities;
+
+namespace Extension_Methods_1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string test = "";
+			Console.WriteLine(test.HasValue());
+		}
+	}
+}
+
+namespace Utilities
+{
+	public static class MyUtilities
+	{
+		public static bool HasValue(this string input)
+		{
+			if (String.IsNullOrWhiteSpace(input))
+			{
+				return false;
+			}
+			else
+			{
+				return true;
+			}
+		}
+	}
+}
