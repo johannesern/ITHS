58e4b83e33a3c1a4b7ef46bd85caf86e04194327
[Johannes Ragnarsson] <[johannes.ragnarsson@iths.se]>
2022-09-21 07:02:21 +0200

    added Extensions Methods 2

---
+++ Extension Methods 2/Program.cs
@@ -0,0 +1,25 @@
+﻿using Utilities;
+
+namespace Extension_Methods_2
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string test = "Jag älskar att köra motorcykel!";
+			Console.WriteLine(test.WordCount().ToString());
+		}
+	}
+}
+
+namespace Utilities
+{
+	public static class MyUtilities
+	{
+		public static int WordCount(this string input)
+		{
+			string[] strings = input.Split(' ');
+			return strings.Length;
+		}
+	}
+}
