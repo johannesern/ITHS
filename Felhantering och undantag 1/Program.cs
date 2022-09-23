a573dd8319e7a9414ba8edf48a85d5a6bb6de620
[Johannes Ragnarsson] <[johannes.ragnarsson@iths.se]>
2022-09-23 10:55:51 +0200

    added try-catch practice

---
+++ Felhantering och undantag 1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Felhantering_och_undantag_1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			try
+			{
+				Console.Write("Ange din inkomst: ");
+				int inkomst = int.Parse(Console.ReadLine());
+				Console.Write("Ange antal timmar: ");
+				int timmar = int.Parse(Console.ReadLine());
+
+				Console.WriteLine("Din timlön blev: " + (inkomst / timmar) + " kr/h");
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex);
+			}
+		}
+	}
+}
