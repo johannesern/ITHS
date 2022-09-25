3fbfdf01fa4a4777eb056bff5072382df71fb39f
Johannes Ragnarsson <johannes.ragnarsson@gmail.com>
2022-09-25 11:18:02 +0200

    added Felhantering och undantag 6

---
+++ Felhantering och undantag 6/Felhantering och undantag 6/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace Felhantering_och_undantag_6
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			ProgramStart();
+		}
+
+		public static void ProgramStart()
+		{
+			int inkomst;
+			int timmar;
+			bool inmatning = false;
+			do
+			{
+				Console.Write("Ange din inkomst: ");
+				inmatning = int.TryParse(Console.ReadLine(), out inkomst);
+				if (!inmatning)
+				{
+					Console.WriteLine("Endast heltal och inget annat och försök inte dela med noll.");
+					inmatning = false;
+				}
+				else
+				{
+					inmatning = true;
+				}
+			} while (!inmatning);
+
+			do
+			{
+				Console.Write("Ange antal timmar: ");
+				inmatning = int.TryParse(Console.ReadLine(), out timmar);
+				if (!inmatning)
+				{
+					Console.WriteLine("Endast heltal och inget annat och försök inte dela med noll.");
+					inmatning = false;
+				}
+				else
+				{
+					inmatning = true;
+				}
+			} while (!inmatning);
+
+			bool Excthrow = false;
+			try
+			{
+				Console.WriteLine("Din timlön blev: " + (inkomst / timmar) + " kr/h");
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("Försök inte att dela med noll eller några andra dumheter.\nGör om och gör rätt...");
+				Excthrow = true;
+			}
+			finally
+			{
+				if (Excthrow)
+				{
+					ProgramStart();
+				}
+			}
+		}
+	}
+}
