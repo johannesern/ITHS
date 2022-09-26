c27306146c3e705c718bc617256a69d12d9a54a8
Johannes Ragnarsson <johannes.ragnarsson@gmail.com>
2022-09-26 20:51:18 +0200

    sln Samlingsklasser övning 3

---
+++ Samlingsklasser 3 - Kortlek/Samlingsklasser 3 - Kortlek/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Samlingsklasser_3___Kortlek
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			List<string> kortlek =	CreateCards();
+			bool run = true;
+			Random random = new Random();
+			do
+			{
+				if(kortlek.Count == 0)
+				{
+					run = false;
+				}
+				else
+				{
+					int i = random.Next(0, kortlek.Count);
+					Console.WriteLine(kortlek[i]);
+					kortlek.RemoveAt(i);
+				}
+			}while (run);
+		}
+
+		public static List<string> CreateCards()
+		{
+			List<string> kortlek = new List<string>();
+			string[] kortTyp = new string[] { "h", "k", "r", "s" };
+			foreach (string typ in kortTyp)
+			{
+				foreach (string card in CardColor(typ))
+				{
+					kortlek.Add(card);
+				}
+			}
+			return kortlek;
+		}
+
+		public static string[] CardColor(string x)
+		{
+			string[] arr = new string[13];
+			arr[0] = x + "E";
+			for (int i = 1; i < 10; i++)
+			{
+				arr[i] = x + (i + 1);
+			}
+			arr[10] = x + "Kn";
+			arr[11] = x + "D";
+			arr[12] = x + "K";
+			return arr;
+		}
+	}
+}
