a657938445ca9d46e97dd8490d4d4eec2cb1148d
[Johannes Ragnarsson] <[johannes.ragnarsson@iths.se]>
2022-09-27 07:35:51 +0200

    Samlingsklasser 4 Dra random kort - SOLVED

--- Samlingsklasser/Samlingsklasser 4 - Dictionary/Program.cs
+++ Samlingsklasser/Samlingsklasser 4 - Dictionary/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Globalization;
+using System.Threading;
 
 namespace Samlingsklasser_4___Dictionary
 {
@@ -6,11 +7,32 @@ namespace Samlingsklasser_4___Dictionary
 	{
 		static void Main(string[] args)
 		{
+			//Första random
 			Random random1 = new Random();
+			int firstCard = random1.Next(1, 53);
 			Dictionary<string, int> dictionary = CreateDictionary();
 			Random random2 = new Random();
-			int firstCard = random1.Next(1, 53); //Keyvaluepair någonting, kolla csharpexercise, jämföra 2 kort
-
+			int parCounter = 0;
+			for (int i = 0; i < 1000; i++)
+			{
+				//Andra random efter att programmet fått jobba lite,
+				//ligger båda random direkt efter varandra finns risken
+				//att man får två random tal i serie och inte olika värde.
+				int secondCard = random2.Next(0, 52);
+				if (firstCard != secondCard)
+				{
+					if(dictionary.ElementAt(firstCard).Value == dictionary.ElementAt(secondCard).Value)
+					{
+						Console.WriteLine("PAR!");
+						Console.WriteLine(i);
+						parCounter++;
+					}
+				}
+				firstCard = random1.Next(0, 52);
+			}
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine("\nAv 1000 test erhölls Par " + parCounter + " gånger.");
+			Console.ForegroundColor = ConsoleColor.Gray;
 		}
 
 		public static Dictionary<string, int> CreateDictionary()
